bcb6179b38c826107b2a3aa01fb46fce3d9feb05
eyhn <cneyhn@gmail.com>
2021-02-22 16:44:18 +0800

    ssh-dind

---
+++ ssh-dind/install-docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
